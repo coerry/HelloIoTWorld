f021257f9b8e9cb7662e5b5dd0c30bee7c3aa70a
Ildar <renorq@mail.ru>
2019-11-18 17:40:02 +0100

    before merging

--- app/src/main/java/com/cr/helloiotworld/MainActivity.java
+++ app/src/main/java/com/cr/helloiotworld/MainActivity.java
@@ -1,11 +1,7 @@
 package com.cr.helloiotworld;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Pair;
-import android.view.View;
 import android.widget.EditText;
-import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -103,52 +99,5 @@ public class MainActivity extends AppCompatActivity {
         }
 
         txt.setText(builder.toString());
-    }
-
-    public void btn_click(View view) {
-        switch (view.getId()) {
-            case R.id.add_product_button:
-                db.insertProduct(oldText.getText().toString(), Integer.parseInt(editBrandId.getText().toString()));
-                break;
-            case R.id.add_brand_button:
-                db.insertBrand(oldText.getText().toString());
-                break;
-            case R.id.delete_product_button:
-                db.deleteProduct(oldText.getText().toString());
-                break;
-            case R.id.delete_brand_button:
-                db.deleteBrand(oldText.getText().toString());
-                break;
-            case R.id.update_product_button:
-                db.updateProduct(oldText.getText().toString(),
-                        newText.getText().toString(),
-                        Integer.parseInt(editBrandId.getText().toString()));
-                break;
-            case R.id.update_brand_button:
-                db.updateBrand(oldText.getText().toString(),
-                        newText.getText().toString());
-                break;
-            case R.id.menu_button:
-                Intent intent = new Intent(this, login.class);
-                startActivity(intent);
-                break;
-            case R.id.read_button:
-                textView.setText("");
-
-                Pair<ArrayList<String>, ArrayList<String>> text = db.read();
-                textView.append("Products:\n");
-                for (String str :
-                        text.first) {
-                    textView.append(str + "\n");
-                }
-
-                textView.append("Brands:\n");
-                for (String str :
-                        text.second) {
-                    textView.append(str + "\n");
-                }
-
-                break;
-        }
     }
 }
